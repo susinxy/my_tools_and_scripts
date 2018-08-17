35855eb8c4d2f779d5db8ed46992d845b4f2fb5b
Liuhongduo <36252075+Liuhongduo@users.noreply.github.com>
2018-08-17 17:56:39 +0800

    Update recorder.h
    add function visualization()

--- log/recorder/recorder.h
+++ log/recorder/recorder.h
@@ -1,13 +1,42 @@
 #ifndef _LOG_RECORDER_H
 #define _LOG_RECORDER_H
+#define MaxSize 128
+#define record_node_len sizeof(record_node)
+#define record_element_len sizeof(record_element)
+typedef struct _record_element
+{
+    unsigned short year;
+    unsigned char month;
+    unsigned char day;
+    unsigned char hour;
+    unsigned char min;
+    unsigned char sec;
+    unsigned int type;
+    unsigned int key;
+    unsigned int oper;
+    unsigned int len;
+    char data[0];
+}record_element;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+typedef struct _record_node
+{
+    unsigned char in_use;
+    unsigned int block_offset;
+    unsigned int how_many_blocks;
+}record_node;
+
+typedef struct _section
+{
+    int block_size;
+    void *addr;
+    unsigned int len;
+    unsigned int next_write_address;
+}section;
+
 int record(int section_id,int type, int key, int oper,int para_cnt,...);
 int record_section(int section_id,int block_size,void *addr,int len);
 int record_section_destory(int section_id);
-#ifdef __cplusplus
-}
+
+void visualization(void* addr,int length,int block_size,char* filename);
 #endif
-#endif
+
