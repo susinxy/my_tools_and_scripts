9e3b7c7fb5853f8450d062fc79ee4f5f585385b7
zby199884 <39860345+zby199884@users.noreply.github.com>
2018-08-17 17:23:23 +0800

    Update compressor.c

--- log/compressor/compressor.c
+++ log/compressor/compressor.c
@@ -1,11 +1,10 @@
-int compress(void *addr_in,int len_in, void**addr_out, int *len_out)
+int compress(void *addr_in,int len_in, void*addr_out, int *len_out)
 {
-    *addr_out=addr_in;
     *len_out=len_in/2;
     return 0;
 }
 
-int decompress(void *addr_in,int len_in, void**addr_out, int *len_out)
+int decompress(void *addr_in,int len_in, void*addr_out, int *len_out)
 {
     return 0;
 }
