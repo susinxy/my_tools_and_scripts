213ca424a9bdf4bae5a41b60b15ee92c03105b55
zby199884 <39860345+zby199884@users.noreply.github.com>
2018-08-17 17:18:55 +0800

    Update compressor_test.cc

--- log/test/compressor/compressor_test.cc
+++ log/test/compressor/compressor_test.cc
@@ -1,12 +1,35 @@
 #include "../../gtinclude/gtest/gtest.h"
 #include "../../compressor/compressor.h"
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
 
-char addr_in[1024*1024];
-char addr_out[sizeof(addr_in)];
-TEST(compressor,para_test)
+/*void rand_inital(char *str,int num)
 {
-    int len_out;
-    EXPECT_EQ(0,compress((void*)addr_in,sizeof(addr_in),(void**)addr_out,&len_out));
-    EXPECT_LT(len_out,sizeof(addr_in));
-}
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < num;i++,str++)
+    {
+        *str = rand() % 100 + 28;  
+    }
+}*/
 
+char str[1024];
+char compress_out[sizeof(str)];
+char decompress_out[sizeof(str)];
+TEST(compressor,para_test)
+{   
+    int compress_len_out,decompress_len_out;
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < sizeof(str);i++)
+    {
+        str[i] = rand() % 100 + 28;  
+    }
+    //printf("%s",str); 
+    printf("%s",str);
+    compress(str,sizeof(str),(void*)compress_out,&compress_len_out);
+    decompress(compress_out,compress_len_out,(void*)decompress_out,&decompress_len_out);   
+
+    EXPECT_EQ(0,memcmp(str,decompress_out,decompress_len_out));
+    EXPECT_LT(sizeof(str),decompress_len_out);
+}
