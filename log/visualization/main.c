48251274d79f928dbf64b15c0ca83c5fbe45065f
Ren12345678 <42317921+Ren12345678@users.noreply.github.com>
2018-08-24 16:44:32 +0800

    Update main.c

--- log/visualization/main.c
+++ log/visualization/main.c
@@ -8,12 +8,15 @@ int main() {
 	char* filename2 = malloc(30);
 	printf("请输入你要打开的文件名及路径，如c:\\temp.txt\n");
 	scanf("%s", filename2);
-	FILE* fp2 = fopen(filename2, "r");
-	int len = 0, block_size = 52;
-	char* addr = malloc(40);
-	char* buffer=malloc(256);
-	int count = fread(buffer, 1, sizeof(buffer), fp2);
-	decompress(buffer, count, addr, &len);
-	visualization(addr, len, block_size);
+	FILE* fp2 = fopen(filename2, "r+");
+	int block_size = 52;
+	fseek(fp2, 0L, SEEK_END); /* 定位到文件末尾 */
+	long int flen = ftell(fp2); /* 得到文件大小 */
+	char* p = (char *)malloc(flen + 1); /* 根据文件大小动态分配内存空间 */
+	void* addr = (char*)malloc(5 * flen);
+	decompress(p, flen, addr, &flen);
+	visualization(addr, flen, block_size);
+	free(p);
+	free(addr);
 	return 0;
 }
