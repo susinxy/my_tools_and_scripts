e297dc576a5d072cd8303a00898563f46e45b685
Liuhongduo <ataraxialiu@hotmail.com>
2018-08-28 11:46:54 +0800

    update test file and recorder.c

--- log/test/recorder/recorder_test.cc
+++ log/test/recorder/recorder_test.cc
@@ -24,7 +24,9 @@ char test5[512];
 char test51[512];
 
 char test6[512];
-char test61[512];
+char test7[1024*1024];
+char test8[1024*1024];
+char test9[512];
 
 char s[200]="A0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz";
 char n[100]="B0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz";
@@ -55,6 +57,7 @@ void init_meta_data_section(void *addr,int len,int block_size)
     for(i=0;i<node_cnt;i++)
     {
         node[i].in_use=0;
+	node[i].block_size=block_size;
         node[i].block_offset=i*block_size+node_cnt*sizeof(record_node);
 	if(i==node_cnt-1)
         {
@@ -94,8 +97,8 @@ TEST(recorder,para_test02)
     timess = localtime(&t);
     record(1,1,1,1,1,s,4);
    
-    test21[0]=1;
     test21[2]=1;
+    test21[3]=1;
     record_elements *p=(record_elements*)malloc(52);
 
     p->year=timess->tm_year+1900;
@@ -115,9 +118,9 @@ TEST(recorder,para_test02)
 
     memcpy(test21+96,p,32);  
     memcpy(test21+128,a,4);
-   
+    
     free(p);
-    EXPECT_EQ(0,memcmp((void*)test2,(void*)test21,512));
+    EXPECT_EQ(0,memcmp((void*)test2,(void*)test21,96));
 }
 
 TEST(recorder,para_test03)
@@ -128,9 +131,10 @@ TEST(recorder,para_test03)
     timess = localtime(&t);
     init_meta_data_section((void*)test31,sizeof(test31),52);
     record(1,2,2,2,1,s,30);
-    test31[0]=1;
-    test31[2]=2;
-    test31[12]=1;
+
+    test31[2]=1;
+    test31[3]=2;
+    test31[14]=1;
     record_elements *p=(record_elements*)malloc(116);
     p->year=timess->tm_year+1900;
     p->month=timess->tm_mon+1;
@@ -166,16 +170,16 @@ TEST(recorder,para_test04)
     record(1,3,3,3,3,s,100,n,50,l,50);
     record(1,4,4,4,1,n,50);
     
-    test41[0]=1;
-    test41[12]=1;
-    test41[24]=1;
-    test41[26]=5;
-    test41[36]=1;
-    test41[48]=1;
-    test41[60]=1;
-    test41[72]=1;
-    test41[84]=1;
-    test41[86]=2;
+    test41[2]=1;
+    test41[14]=1;
+    test41[26]=1;
+    test41[27]=5;
+    test41[38]=1;
+    test41[50]=1;
+    test41[62]=1;
+    test41[74]=1;
+    test41[86]=1;
+    test41[87]=2;
     
     record_elements *p=(record_elements*)malloc(512);
     record_elements *q=(record_elements*)malloc(512);
@@ -195,10 +199,12 @@ TEST(recorder,para_test04)
     
     char *b = "A0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopq";
     char *c=  "B0123456789abcdefghijklmnopqrstuvwxyz0123456789abc";
-    memcpy(test41+96+52,b+20,10);
+    
     memcpy(test41+460,p,32);
     memcpy(test41+460+32,c,20);
     memcpy(test41+96,c+20,30);
+
+    memcpy(test41+96+52,b+20,10);
     
    
     q->year=timess->tm_year+1900;
@@ -221,17 +227,9 @@ TEST(recorder,para_test04)
     memcpy(test41+336,c,50);
     memcpy(test41+386,&datalen,4);
     memcpy(test41+390,c,50);
-    for(int i=460;i<468;i++)
-    {   printf("%d:%d",i,memcmp((void*)test4+i+1,(void*)test41+i+1,1));
-    	printf("%c",test4[i]);
-        printf("%c",test41[i]);
-        printf("\n");
-    }
-   
     free(p);
-    EXPECT_EQ(0,memcmp((void*)test4,(void*)test41,460));
-    EXPECT_EQ(0,memcmp((void*)test4+468,(void*)test41+468,44));
- 
+    EXPECT_EQ(0,memcmp((void*)test4,(void*)test41,512));
+    
 }
 
 TEST(recorder,para_test05)
@@ -246,16 +244,16 @@ TEST(recorder,para_test05)
     record(1,4,4,4,1,n,50);
     record(1,5,5,5,1,s,100);
     
-    test51[0]=1;
-    test51[12]=1;
-    test51[24]=1;
-    test51[36]=1;
-    test51[48]=1;
-    test51[60]=1;
-    test51[72]=1;
-    test51[84]=1;
-    test51[14]=3;
-    test51[86]=2;
+    test51[2]=1;
+    test51[14]=1;
+    test51[26]=1;
+    test51[38]=1;
+    test51[50]=1;
+    test51[62]=1;
+    test51[74]=1;
+    test51[86]=1;
+    test51[15]=3;
+    test51[87]=2;
     
     int datalen=50;
     char *b = "A0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopq";
@@ -300,23 +298,37 @@ TEST(recorder,para_test05)
     memcpy(test51+390,c,50);
     memcpy(test51+460,q,32);
     memcpy(test51+460+32,c,20);
-
-    for(int i=148;i<156;i++)
-    {   printf("%d:%d",i,memcmp((void*)test5+i+1,(void*)test51+i+1,1));
-    	printf("%c",test5[i]);
-        printf("%c",test51[i]);
-        printf("\n");
-    }
-    for(int i=460;i<468;i++)
-    {   printf("%d:%d",i,memcmp((void*)test5+i+1,(void*)test51+i+1,1));
-    	printf("%c",test5[i]);
-        printf("%c",test51[i]);
-        printf("\n");
-    }
+    EXPECT_EQ(0,memcmp((void*)test5,(void*)test51,512));
     
-    EXPECT_EQ(0,memcmp((void*)test5,(void*)test51,148));
-    EXPECT_EQ(0,memcmp((void*)test5+156,(void*)test5+156,304));
-    EXPECT_EQ(0,memcmp((void*)test5+468,(void*)test51+468,44));
+}
+
+
+
+TEST(recorder,para_test06)
+{
+    if( record_section(-1,52,test6,256) == -1 ) 
+	printf("Record_Section Create Failure:section_id should be in the range of 0~127\n");
+}
+
+TEST(recorder,para_test07)
+{
+    if( record_section(1,256*512,test7,1024*1024) == -2 ) 
+	printf("Record_Section Create Failure:block_size should be in the range of 0~65535\n");
+}
+
+TEST(recorder,para_test08)
+{
+    record_section(1,1,test8,256*256);
+    if( record(1,6,6,6,3,s,100,n,50,l,100) == -2 ) 
+	printf("Record Failure:how_many_block should be in the range of 0~255\n");
+}
+  
+
+TEST(recorder,para_test09)
+{
+    record_section(1,52,test9,256);
+    if( record(1,6,6,6,3,s,100,n,50,l,50) == -1 ) 
+	printf("Record Failure:the total size of block is not enough for the record\n");
 }
   
 
