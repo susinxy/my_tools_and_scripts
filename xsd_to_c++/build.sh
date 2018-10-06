1a9eedd98f3644cff61cb20901c6616ef1e41390
susinxy <susinxy@sina.com>
2018-10-06 21:00:12 +0800

    xml

--- xsd_to_c++/build.sh
+++ xsd_to_c++/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-gengen -i struct_def.txt --file-name struct_def.h --gen-name struct_def
-gengen -i members_gen.txt --file-name members_gen.h --gen-name members_gen 
-gcc SCMPrint.cpp -o SCMPrint -lxerces-c -lstdc++
+script_dir=$(cd `dirname $0`;pwd)
+gengen -i $script_dir/struct_def.txt --output-dir $script_dir --file-name struct_def.h --gen-name struct_def
+gengen -i $script_dir/members_gen.txt --output-dir $script_dir --file-name members_gen.h --gen-name members_gen 
+gcc $script_dir/SCMPrint.cpp -o $script_dir/SCMPrint -lxerces-c -lstdc++
